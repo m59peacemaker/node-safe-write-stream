a50f1cc1deb46f3a98b223af335f01b9775dcead
Johnny Hauser <johnnyhauser@gmail.com>
2015-03-20 22:42:57 -0500

    it works now

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 var fs       = require('fs');
 var path     = require('path');
 var mkdirp   = require('mkdirp');
+var PT       = require('stream').PassThrough;
 
 module.exports = function(filepath) {
   var args = arguments;
-  var dirPromise = new Promise(function(resolve, reject) {
+  var dirsCreated = new Promise(function(resolve, reject) {
     mkdirp(path.dirname(filepath), resolve);
   });
-  var stream = //sigh;
+  var fsDoneResolve;
+  var fsDone = new Promise(function(resolve, reject) {
+    fsDoneResolve = resolve;
+  });
+  var stream = new PT();
   stream.pause();
-  dirPromise.then(function() {
+  hijackEmit(stream, fsDone);
+  dirsCreated.then(function() {
+    stream.pipe(fs.createWriteStream.apply(fs, args)).on('finish', fsDoneResolve);
     stream.resume();
-    stream.pipe(fs.createWriteStream.apply(fs, args))
   });
   return stream;
 };
+
+function hijackEmit(stream, fsDone) {
+  var oldEmit = stream.emit;
+  stream.emit = function(type) {
+    var args = arguments;
+    if (type === 'finish') {
+      fsDone.then(function() {
+        oldEmit.apply(stream, args);
+      });
+    } else {
+      oldEmit.apply(stream, args);
+    }
+  };
+}
